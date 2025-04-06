6b7cdaa2b6b6434460c4333c368cc35f21e68e4a
ubuntu <ubuntu@prtcpi.modem>
2025-04-06 18:57:10 +1000

    hello

--- packages/square_turtle_pkg/src/square_turtle.py
+++ packages/square_turtle_pkg/src/square_turtle.py
@@ -1,41 +1,44 @@
 #!/usr/bin/env python3
 
-# Import Dependencies
-import rospy 
-from geometry_msgs.msg import Twist 
-import time 
+import rospy
+from geometry_msgs.msg import Twist
+import math
 
-def move_turtle_square(): 
+LINEAR_SPEED = 2.0
+ANGULAR_SPEED = math.pi 
+TRAVEL_DISTANCE = 2.0
+
+def move_turtle_square():
     rospy.init_node('turtlesim_square_node', anonymous=True)
-    
-    # Init publisher
-    velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10) 
-    rospy.loginfo("Turtles are great at drawing squares!")
+    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    rate = rospy.Rate(10)
 
-    ########## YOUR CODE GOES HERE ##########
+    def move(linear=0.0, angular=0.0, duration=0.0):
+        start = rospy.Time.now().to_sec()
+        msg = Twist()
+        msg.linear.x = linear
+        msg.angular.z = angular
+        while rospy.Time.now().to_sec() - start < duration and not rospy.is_shutdown():
+            pub.publish(msg)
+            rate.sleep()
+        msg.linear.x = 0.0
+        msg.angular.z = 0.0
+        pub.publish(msg)
+        rate.sleep()
+
     while not rospy.is_shutdown():
-        
-        rate = rospy.Rate(1)  # 1 Hz, adjust as needed
-        
-        # Create a Twist message for moving forward 
-        cmd_vel_msg = Twist() 
-        cmd_vel_msg.linear.x = 2.0  # Linear velocity
-        velocity_publisher.publish(cmd_vel_msg) # Publish!
+        move(linear=LINEAR_SPEED, duration=TRAVEL_DISTANCE / LINEAR_SPEED)
+        move(angular=ANGULAR_SPEED, duration= (math.pi +0.5)/ ANGULAR_SPEED)
+        move(linear=LINEAR_SPEED, duration=TRAVEL_DISTANCE / LINEAR_SPEED)
+        move(angular=ANGULAR_SPEED, duration= (math.pi +0.5) / ANGULAR_SPEED)
+        move(linear=LINEAR_SPEED, duration=TRAVEL_DISTANCE / LINEAR_SPEED)
+        move(angular=ANGULAR_SPEED, duration= (math.pi+0.5) / ANGULAR_SPEED)
+        move(linear=LINEAR_SPEED, duration=TRAVEL_DISTANCE / LINEAR_SPEED)
+        move(angular=ANGULAR_SPEED, duration= (math.pi +0.5) / ANGULAR_SPEED)
 
-        rate.sleep()
 
-        # Create a Twist message for moving backward 
-        cmd_vel_msg = Twist()
-        cmd_vel_msg.linear.x = -2.0  # Linear velocity
-        velocity_publisher.publish(cmd_vel_msg) # Publish!
-
-        rate.sleep()
-
-        ###########################################
-
-if __name__ == '__main__': 
-
-    try: 
-        move_turtle_square() 
-    except rospy.ROSInterruptException: 
+if __name__ == '__main__':
+    try:
+        move_turtle_square()
+    except rospy.ROSInterruptException:
         pass
